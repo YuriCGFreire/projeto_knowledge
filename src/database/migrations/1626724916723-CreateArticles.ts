c234634f2e6573f07b75e86fb62ff6b5a23ff698
Yuri Freire <yurifreireg@gmail.com>
2021-07-19 17:21:14 -0300

    Create 1626724916723-CreateArticles.ts

---
+++ src/database/migrations/1626724916723-CreateArticles.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateArticles1626724916723 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
