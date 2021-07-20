c4699e63970429bf3afd2a032a475b6de3ba2163
Yuri Freire <yurifreireg@gmail.com>
2021-07-20 14:27:19 -0300

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm"
 import {v4 as uuid} from "uuid"
+import { IsEmail, MaxLength, MinLength } from "class-validator"
 
 @Entity()
 export class User{
@@ -7,12 +8,16 @@ export class User{
     id!: string;
 
     @Column()
+    @MinLength(3, {message: "Um nome precisa ter pelo menos 3 carácteres."})
+    @MaxLength(50, {message: "Um nome precisa ter no max 50 carácteres."})
     name!: string;
 
     @Column()
+    @IsEmail()
     email!: string;
 
     @Column()
+    @MinLength(6, {message: "A senha precisa ter no mínimo 6 carácteres."})
     password!: string;
 
     @Column()
