5b38fd5d1a8c0f2de91569a8b662cd44d0c8844a
Yuri Freire <yurifreireg@gmail.com>
2021-07-20 15:24:47 -0300

    Update UsersService.ts

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,9 +1,12 @@
 import { UserRepository,} from "../repositories/UserRepository";
 import {Repository, getCustomRepository} from "typeorm"
+import { validate } from "class-validator";
 import { User } from "../models/User";
 
 interface IUsersInterface{
-    
+    name: string,
+    email: string,
+    password: string
 }
 
 export class UsersService{
@@ -13,8 +16,25 @@ export class UsersService{
         this.usersRepository = getCustomRepository(UserRepository)
     }
 
-    async create(){
+    async create({name, email, password}: IUsersInterface){
 
+        const userExists = await this.usersRepository.findOne({ email })
+        if(userExists){
+            const msg = `Usuário já cadastrado.`
+            return msg
+        }
+
+        const user = this.usersRepository.create({
+            name,
+            email, 
+            password
+        })
+
+        const errors = await validate(user)
+        if(errors.length == 0){
+            await this.usersRepository.save(user)
+            return user
+        }
 
     }
 }
