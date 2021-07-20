d97852e89b0604d9b473a38727a5c39efe703978
Yuri Freire <yurifreireg@gmail.com>
2021-07-19 22:08:19 -0300

    Create Article.ts

---
+++ src/models/Article.ts
@@ -0,0 +1,48 @@
+import { Binary, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryColumn, UpdateDateColumn } from "typeorm"
+import { v4 as uuid } from "uuid"
+import { User } from "./User";
+
+@Entity("articles")
+export class Article{
+    @PrimaryColumn()
+    id!: string;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    description!: string;
+
+    @Column()
+    image_url!: string;
+
+    @Column()
+    content!: Binary;
+
+    @JoinColumn({ name: "user_id" })
+    @ManyToOne(type => User, type => Article)
+    user!: User;
+
+    @Column()
+    user_id!: string;
+
+    @JoinColumn({ name: "category_id" })
+    @OneToOne(type => Article)
+    article!: string;
+
+    @Column()
+    article_id!: string;
+
+    @CreateDateColumn()
+    created_at!: Date;
+
+    @UpdateDateColumn()
+    updated_at!: Date;
+
+    constructor(){
+        if(!this.id){
+            this.id = uuid()
+        }
+    }
+    
+}
