f83cb2596cbd29c9878466cb2c2485d36fb6210d
Yuri Freire <yurifreireg@gmail.com>
2021-07-21 16:12:53 -0300

    Update UsersController.ts

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -4,16 +4,11 @@ import { UsersService } from "../services/UsersService";
 export class UsersController {
 
     async save(req: Request, res: Response): Promise<Response>{
-        const {name, email, password, confirmPassword} = req.body
+        const { name, email, password, admin = null } = req.body
         const usersService = new UsersService()
-        
-        if(password != confirmPassword){
-            res.json({"Message: ": "Senhas não conferem."})
-        }
-
         try{
-            const user = await usersService.create({ name, email, password })
-            return res.json({user})            
+            const user = await usersService.create({name, email, password, admin})
+            return res.json(user)            
         }catch(err){
             return res.json(err.message)
         }
