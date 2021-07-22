2bc28c412b3935f134afe2a0802fb235376baba9
Yuri Freire <yurifreireg@gmail.com>
2021-07-22 14:44:02 -0300

    Update CategoriesService.ts

--- src/services/CategoriesService.ts
+++ src/services/CategoriesService.ts
@@ -1,1 +1,32 @@
+import { Repository, getCustomRepository } from "typeorm";
 import { Category } from "../models/Category";
+import { CategoryRepository } from "../repositories/CategoryRepository";
+import { validate } from "class-validator";
+
+export class CategoriesService {
+    private categoryRepository: Repository<Category>
+
+    constructor(){
+        this.categoryRepository = getCustomRepository(CategoryRepository)
+    }
+
+    async create(name: string){
+
+        const categoryExists = await this.categoryRepository.find({ name })
+        if(categoryExists){
+            const msg = "Categoria já cadastrada."
+            return msg
+        }
+
+        const category = this.categoryRepository.create({name})
+
+        const errors = await validate(category)
+        if(errors.length == 0){
+            await this.categoryRepository.save(category)
+            return category
+        }else{
+            return errors
+        }
+
+    }
+}
