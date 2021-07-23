1b8bb20a432b7ae8d08602a06cb2f70914e060e3
Yuri Freire <yurifreireg@gmail.com>
2021-07-22 22:22:25 -0300

    Update CategoriesService.ts

--- src/services/CategoriesService.ts
+++ src/services/CategoriesService.ts
@@ -2,24 +2,25 @@ import { Repository, getCustomRepository } from "typeorm";
 import { Category } from "../models/Category";
 import { CategoryRepository } from "../repositories/CategoryRepository";
 import { validate } from "class-validator";
+import { Article } from "../models/Article";
+import { ArticleRepository } from "../repositories/ArticleRepository";
 
 export class CategoriesService {
     private categoryRepository: Repository<Category>
+    private articleRepository: Repository<Article>
 
     constructor(){
         this.categoryRepository = getCustomRepository(CategoryRepository)
+        this.articleRepository = getCustomRepository(ArticleRepository)
     }
 
     async create(name: string){
-
         const categoryExists = await this.categoryRepository.findOne({ name })
         if(categoryExists){
             const msg = "Categoria já cadastrada."
             return msg
         }
-
         const category = this.categoryRepository.create({name})
-
         const errors = await validate(category)
         if(errors.length == 0){
             await this.categoryRepository.save(category)
@@ -27,7 +28,6 @@ export class CategoriesService {
         }else{
             return errors
         } 
-
     }
 
     async remove (id: string){
@@ -37,25 +37,26 @@ export class CategoriesService {
             where: {parent_id: id}
         })
 
-        //conts article 
+        const article = await this.articleRepository.find({
+            select: ["article_id"],
+            where: {article_id: id}
+        })  
 
         if(subCategory.length != 0){
             const msg = "Categoria possui subcategorias."
             return msg
+        }else if(article.length != 0){
+            const msg = "Categoria possui artigos."
+            return msg
         }else{
-            const msg = "Categoria deletada."
-            
+            const msg = "Categoria deletada."            
             await this.categoryRepository
                 .createQueryBuilder()
                 .delete()
                 .from(Category)
                 .where({id})
-                .execute()
-    
+                .execute() 
             return msg
-
-        }
-        //Depois implementar a verificação de artigos, antes de poder deletar a categoria.
-        //Obs: Implementar antes do else     
+        }   
     }
 }
