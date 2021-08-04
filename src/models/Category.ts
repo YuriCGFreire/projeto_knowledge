c3c33cde38fcf555cff38c0f173a615200ca0513
Yuri Freire <yurifreireg@gmail.com>
2021-08-04 17:45:33 -0300

    Update Category.ts

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, Tree,Entity, PrimaryColumn, UpdateDateColumn, TreeParent, TreeChildren } from "typeorm"
+import { Column, CreateDateColumn, Tree,Entity, PrimaryColumn, UpdateDateColumn, TreeParent, TreeChildren, OneToMany, JoinColumn } from "typeorm"
 import { v4 as uuid } from "uuid"
 import { MaxLength, MinLength } from "class-validator"
+import { Article } from "./Article";
 
 @Entity("categories")
 export class Category{
@@ -12,6 +13,10 @@ export class Category{
     @MinLength(2, {message: "Não atende à quantidade mínima de 2 carácteres."})
     @MaxLength(50, {message: "Ultrapassou a quantidade máxima de 50 carácteres."})
     name!: string;
+
+    @OneToMany(type => Article, category => Category)
+    @JoinColumn()
+    category!: Category;
 
     @CreateDateColumn()
     created_at!: Date;
