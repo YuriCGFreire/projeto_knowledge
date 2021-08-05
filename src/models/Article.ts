5699117df2ab5f5fae3d2d1ea37b1ba79c6df805
Yuri Freire <yurifreireg@gmail.com>
2021-08-05 17:17:29 -0300

    Update Article.ts

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -1,18 +1,24 @@
 import {Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryColumn, UpdateDateColumn } from "typeorm"
 import { v4 as uuid } from "uuid"
+
 import { Category } from "./Category";
 import { User } from "./User";
 import { Coment } from "./Coment";
+import { MaxLength, MinLength } from "class-validator";
 
 @Entity("articles")
 export class Article{
-    @PrimaryColumn()
+    @PrimaryColumn({type: "uuid"})
     id!: string;
 
     @Column()
+    @MinLength(2, {message: "Não atende à quantidade mínima de 2 carácteres."})
+    @MaxLength(20, {message: "Ultrapassou a quantidade máxima de 20 carácteres."})
     name!: string;
 
     @Column()
+    @MinLength(10, {message: "Não atende à quantidade mínima de 10 carácteres."})
+    @MaxLength(120, {message: "Ultrapassou a quantidade máxima de 120 carácteres."})
     description!: string;
 
     @Column()
