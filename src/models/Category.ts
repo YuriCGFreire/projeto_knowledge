9cf690c30530e2a4cc7b3120bacf3c6973649844
Yuri Freire <yurifreireg@gmail.com>
2021-08-05 17:17:36 -0300

    Update Category.ts

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Tree,Entity, PrimaryColumn, UpdateDateColumn, TreeParent, TreeChildren, OneToMany, JoinColumn } from "typeorm"
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn, TreeParent, TreeChildren, OneToMany, JoinColumn } from "typeorm"
 import { v4 as uuid } from "uuid"
 import { MaxLength, MinLength } from "class-validator"
 import { Article } from "./Article";
