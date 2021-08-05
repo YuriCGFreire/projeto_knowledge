4d174db409faf4ad12a415ebdf55b2f5e32ae26f
Yuri Freire <yurifreireg@gmail.com>
2021-08-05 17:17:54 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { articleRouter } from "./articleRouter";
 import { categoryRouter } from "./categoryRouter";
 import { userRouter } from "./userRouter";
 const routes = Router()
 
 routes.use("/user", userRouter)
 routes.use("/category", categoryRouter)
+routes.use("/article", articleRouter)
 
 export {routes}
