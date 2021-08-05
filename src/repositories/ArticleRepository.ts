da8d18e1de99fa4d8804028c17ca2ab6bcf46a5b
Yuri Freire <yurifreireg@gmail.com>
2021-08-05 17:17:43 -0300

    Update ArticleRepository.ts

--- src/repositories/ArticleRepository.ts
+++ src/repositories/ArticleRepository.ts
@@ -1,5 +1,7 @@
 import {Repository, EntityRepository} from "typeorm"
 import { Article } from "../models/Article"
-
+ 
 @EntityRepository(Article)
-export class ArticleRepository extends Repository<Article>{}
+export class ArticleRepository extends Repository<Article>{
+    
+}
