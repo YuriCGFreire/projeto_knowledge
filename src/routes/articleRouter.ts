ba94f5835fae7c3fa67e029ee1208a37556d57a5
Yuri Freire <yurifreireg@gmail.com>
2021-08-06 22:51:24 -0300

    Update articleRouter.ts

--- src/routes/articleRouter.ts
+++ src/routes/articleRouter.ts
@@ -3,6 +3,7 @@ import { ArticlesController } from "../controllers/ArticlesController";
 const articleRouter = Router()
 const articlesController = new ArticlesController()
 
+articleRouter.delete('/:id', articlesController.remove)
 articleRouter.post('/', articlesController.createOrUpdate)
 articleRouter.get('/', articlesController.getArticles)
 articleRouter.put("/:id", articlesController.createOrUpdate)
