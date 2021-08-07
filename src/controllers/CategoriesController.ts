34003117c1f84edc86f7cf16ea1f5d89ff1cdae2
Yuri Freire <yurifreireg@gmail.com>
2021-08-06 23:51:29 -0300

    Update CategoriesController.ts

--- src/controllers/CategoriesController.ts
+++ src/controllers/CategoriesController.ts
@@ -1,14 +1,26 @@
 import { CategoriesService } from "../services/CategoriesService";
 import { Request, Response } from "express";
+import { ObjectFlags } from "typescript";
 
 export class CategoriesController {
 
-    async save(req: Request, res: Response): Promise<Response>{
-
+    async createOrUpdate(req: Request, res: Response): Promise<Response>{
         const objCategory = {...req.body}
         const categoriesService = new CategoriesService()
+        const {id} = req.params
+
+        if(req.params.id){
+            objCategory.id = id
+            try{
+                const category = await categoriesService.createOrUpdate(objCategory)
+                return res.json(category)
+            }catch(err){
+                return res.json(err.message)
+            }
+        }
+
         try{
-            const category = await categoriesService.create(objCategory, objCategory.name)
+            const category = await categoriesService.createOrUpdate(objCategory)
             return res.json(category)
         }catch(err){
             return res.json(err.message)
@@ -38,21 +50,11 @@ export class CategoriesController {
     }
 
     async getById(req: Request, res: Response): Promise<Response>{
-        const { id } = req.body
+        const { id } = req.params
+
         const categoriesService = new CategoriesService()
         try{
             const category = await categoriesService.getById(id)
-            return res.json(category)
-        }catch(err){
-            return res.json(err.message)
-        }
-    }
-
-    async updateCategory(req: Request, res: Response): Promise<Response>{
-        const { name, id } = req.body
-        const categoriesService = new CategoriesService()
-        try{
-            const category = await categoriesService.update(name, id)
             return res.json(category)
         }catch(err){
             return res.json(err.message)
