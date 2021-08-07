7120d8e9e811033b2918b284f56b2939aec01971
Yuri Freire <yurifreireg@gmail.com>
2021-08-06 22:51:31 -0300

    Update categoryRouter.ts

--- src/routes/categoryRouter.ts
+++ src/routes/categoryRouter.ts
@@ -3,9 +3,9 @@ import { Router } from "express";
 const categoryRouter = Router()
 const categoriesController = new CategoriesController()
 
-categoryRouter.get("/byid", categoriesController.getById)
+categoryRouter.get("/:id", categoriesController.getById)
 categoryRouter.post("/", categoriesController.save)
-/* categoryRouter.delete("/:id", categoriesController.delete) */
+categoryRouter.delete("/:id", categoriesController.delete)
 categoryRouter.get("/", categoriesController.getCategories)
 categoryRouter.put("/", categoriesController.updateCategory)
 
