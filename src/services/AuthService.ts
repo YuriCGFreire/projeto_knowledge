cfd74180d7ec2c349239b0a65bda6d90a886a523
Yuri Freire <yurifreireg@gmail.com>
2021-08-13 16:09:44 -0300

    Create AuthService.ts

---
+++ src/services/AuthService.ts
@@ -0,0 +1,58 @@
+require("dotenv").config()
+import {decode, encode} from "jwt-simple"
+import { compareSync } from "bcrypt-nodejs"
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../models/User"
+import { UserRepository } from "../repositories/UserRepository"
+
+
+interface IUserSignIn{
+    email:string,
+    password:string
+}
+
+export class AuthService{
+    
+    private userRepository: Repository<User>
+
+    constructor(){
+        this.userRepository = getCustomRepository(UserRepository)
+    }
+
+    async signin({email, password}:IUserSignIn){
+        const user = await this.userRepository.findOne({
+            where: {email: email}
+        })
+
+        if(!user){
+            const msg = "Usuário não cadastrado."
+            return msg
+        }
+
+        const isMatch = compareSync(password, user.password)
+        if(!isMatch){
+            const msg = "Senha/Email inválidos."
+            return msg
+        }
+
+        const now = Math.floor(Date.now() / 1000)
+
+        const payload = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            admin: user.admin,
+            iat: now,
+            exp: now + (60 * 60 * 24 * 5)
+        }
+
+        return {
+            ...payload,
+            token: encode(payload, process.env.AUTH_SECRET as any)
+        }
+    }
+
+    async validateToken(pay)
+
+}
+
