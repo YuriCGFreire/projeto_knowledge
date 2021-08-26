7f4416b7e8df3881d6feff9273289055bb62b0b2
Yuri Freire <yurifreireg@gmail.com>
2021-08-25 22:02:01 -0300

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,37 +7,37 @@ import { Article } from "./Article";
 @Entity("users")
 export class User{
     @PrimaryColumn()
-    id!: string;
+    id: string;
 
     @Column()
     @MinLength(3, {message: "Não atende a quantidade mínima de 3 carácteres."})
     @MaxLength(50, {message: "Ultrapassou a quantidade máxima de carácteres."})
-    name!: string;
+    name: string;
 
     @Column()
     @IsEmail()
-    email!: string;
+    email: string;
 
     @Column()
     @MinLength(8, {message: "Não atende a quantidade mínima de 8 carácteres."})
-    password!: string;
+    password: string;
 
     @OneToMany(type => Coment, user => User)
     @JoinColumn()
-    coment!: Coment;
+    coment: Coment;
 
     @OneToMany(type => Article, user => User)
     @JoinColumn()
-    article!: Article;
+    article: Article;
 
     @Column()
-    admin!: boolean;
+    admin: boolean;
 
     @CreateDateColumn()
-    created_at!: Date;
+    created_at: Date;
 
     @UpdateDateColumn()
-    updated_at!: Date;
+    updated_at: Date;
 
     constructor(){
         if(!this.id){
