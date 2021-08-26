e2381ab75c882c613548f9d948b367af4a62260a
Yuri Freire <yurifreireg@gmail.com>
2021-08-25 22:01:40 -0300

    Update AuthController.ts

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,17 +4,18 @@ import { AuthService } from "../services/AuthService";
 export class AuthController{
 
     async signin(req: Request, res: Response): Promise<Response>{
-        const userData = {...req.body}
+        const {email, password} = req.body
         const authService = new AuthService()
-        if(!userData.email || !userData.password){
+        if(!email || !password){
             return res.json("Email ou senha inválidos.")
         }
+
         try{
-            const signin = await authService.signin(userData)
-            return res.json(signin)
+            const userData = await authService.signin({email, password})
+            return res.json(userData)
         }catch(error){
             return res.json(error.message)
         }
     }
-    
+
 }
