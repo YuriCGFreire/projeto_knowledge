cc64cb4c734a0655d1d9d9084533772bc8362a4e
Yuri Freire <yurifreireg@gmail.com>
2021-08-27 16:27:25 -0300

    Update userRouter.ts

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,14 @@
 import { UsersController } from "../controllers/UsersController"
 import { Router } from "express"
 const userRouter = Router()
+const signupRouter = Router()
 const usersController = new UsersController()
+
+signupRouter.post("/", usersController.save)
 
 userRouter.post('/', usersController.save)
 userRouter.get('/', usersController.getAllUsers)
 userRouter.get('/:email', usersController.getByEmail)
 userRouter.put('/:id', usersController.updateUser)
 
-export {userRouter}
+export {userRouter, signupRouter}
