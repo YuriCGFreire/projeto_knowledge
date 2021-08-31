9e4eaa0ec81a1d9b7b39c8eef88ee1087d89da45
Yuri Freire <yurifreireg@gmail.com>
2021-08-31 13:02:28 -0300

    Update userRouter.ts

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,14 +1,21 @@
 import { UsersController } from "../controllers/UsersController"
 import { Router } from "express"
+import adminMiddleware from "../middlewares/adminMiddleware"
 const userRouter = Router()
 const signupRouter = Router()
 const usersController = new UsersController()
 
+//Não precisa estar logado
 signupRouter.post("/", usersController.save)
 
-userRouter.post('/', usersController.save)
-userRouter.get('/', usersController.getAllUsers)
-userRouter.get('/:email', usersController.getByEmail)
+
+//Precisa estar logado
 userRouter.put('/:id', usersController.updateUser)
+
+//Precisa ser admin
+userRouter.post('/', adminMiddleware(usersController.save))
+userRouter.get('/', adminMiddleware(usersController.getAllUsers))
+userRouter.get('/:email', adminMiddleware(usersController.getByEmail))
+userRouter.delete('/:id', adminMiddleware(usersController.softDeleteUser))
 
 export {userRouter, signupRouter}
