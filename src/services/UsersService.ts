94b2ce1875afd301a20073d179653c2d7b4fdf9f
Yuri Freire <yurifreireg@gmail.com>
2021-08-31 13:02:34 -0300

    Update UsersService.ts

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -2,6 +2,8 @@ import { UserRepository,} from "../repositories/UserRepository";
 import {Repository, getCustomRepository} from "typeorm"
 import { User } from "../models/User";
 import { validate } from "class-validator";
+import { Article } from "../models/Article";
+import { ArticleRepository } from "../repositories/ArticleRepository";
 
 interface IUserInterface{
     name: string, 
@@ -12,9 +14,11 @@ interface IUserInterface{
 
 export class UsersService{
     private usersRepository: Repository<User>
+    private articlesRepositore: Repository<Article>
 
     constructor(){
         this.usersRepository = getCustomRepository(UserRepository)
+        this.articlesRepositore = getCustomRepository(ArticleRepository)
     }
 
     async create({name, email, password, admin}: IUserInterface){
@@ -53,7 +57,8 @@ export class UsersService{
         const [users, total] = await this.usersRepository.findAndCount({
             select: ["name", "email", "admin", "id"],
             take: limit, //número máximo de entidades que devem ser retornado
-            skip: page * limit - limit //a partir de qual entidade as outras entidades devem ser carregadas
+            skip: page * limit - limit, //a partir de qual entidade as outras entidades devem ser carregadas
+            where: {deleted_at: null}
         })
 
         return {data: users, limit, count: total, page: page}
@@ -62,7 +67,7 @@ export class UsersService{
     async getByEmail(email: string){
         const user = await this.usersRepository.find({
             select: ["name", "email", "admin", "id"],
-            where: {email}
+            where: {email: email, deleted_at: null}
         })
 
         return user
@@ -82,5 +87,34 @@ export class UsersService{
         })
 
         return user
+    }
+
+    async softDeleteUser(id: string){
+        const user = await this.usersRepository.find({
+            where: {id}
+        })
+
+        const articles = await this.articlesRepositore.findOne({
+            where: {user_id: id}
+        })
+
+        if(!user){
+            const msg = "Usuário não cadastrado" 
+            return msg
+        }
+        
+        if(articles){
+            const msg = "Usuário possui artigos."
+            return msg
+        }
+        
+        const msg = "Usuário excluido."
+        await this.usersRepository
+            .createQueryBuilder()
+            .softDelete()
+            .where({ id })
+
+        return msg
+
     }
 }
